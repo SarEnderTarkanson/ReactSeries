6d8814c619155ef0fbe9e60126f5ef44c47736a4
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-01-03 18:16:46 +0100

    added some simple event handlers to some buttons

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -4,9 +4,29 @@ const Content = () => {
     const int = Math.floor(Math.random() * 3);
     return names[int];
   };
+
+  const handleClick = () => {
+    console.log("you clicked it");
+  };
+  const handleClick2 = (name) => {
+    console.log(`${name} was  clicked!`);
+  };
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
       <p>Hello {handleNameChange()} </p>
+      <button onClick={handleClick}>Click it!</button>
+      <button
+        onClick={() => {
+          handleClick2("Alpy");
+        }}
+      >
+        Click it!
+      </button>
+      <button onClick={(e) => handleClick3(e)}>Click it!</button>
     </main>
   );
 };
