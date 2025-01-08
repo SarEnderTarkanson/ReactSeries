27744e3ac5b92bec1ac83ffaf773924c3b10303c
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-01-08 01:47:15 +0100

    created Button and Form components

---
+++ fetch-api/src/Form.js
@@ -0,0 +1,9 @@
+import Button from './Button'
+
+const Form = ({ reqType, setReqType }) => {
+  return <form onSubmit={(e) => e.preventDefault()}>
+    <Button></Button>
+  </form>;
+};
+
+export default Form;
