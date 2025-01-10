20c41a7a4bbd358206ba24b0afac78dd507a6644
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-01-10 03:32:11 +0100

    created the boiler plate children components and the main navigation in App.js

--- r-blog-project/src/App.js
+++ r-blog-project/src/App.js
@@ -6,11 +6,23 @@ import NewPost from "./NewPost";
 import PostPage from "./PostPage";
 import About from "./About";
 import Missing from "./Missing";
-import { Route, Switch, useHistory } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post" element={<NewPost />} />
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
