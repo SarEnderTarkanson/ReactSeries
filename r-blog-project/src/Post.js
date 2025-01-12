59460ac72706f046941408ed1c60320b63e81c97
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-01-12 02:44:18 +0100

    Gave the final shape to the Footer, About and Missing components

--- r-blog-project/src/Post.js
+++ r-blog-project/src/Post.js
@@ -8,7 +8,7 @@ const Post = ({ post }) => {
         <p className="postDate">{post.datetime}</p>
       </Link>
       <p className="postBody">
-        {post.body.length <= 25 ? post.body : `${post.body.slice()}...`}
+        {post.body.length <= 25 ? post.body : `${post.body.slice(0, 25)}...`}
       </p>
     </article>
   );
