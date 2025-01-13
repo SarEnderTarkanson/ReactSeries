db0a22e102106650dfa74b2dc9cb5dccfaf1b963
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-01-13 20:21:36 +0100

    created useAxiosFetch custom hook

--- r-blog-project/src/hooks/useWindowSize.js
+++ r-blog-project/src/hooks/useWindowSize.js
@@ -13,13 +13,10 @@ const useWindowSize = () => {
       });
     };
     handleResize();
+
     window.addEventListener("resize", handleResize);
 
-    const cleanUp = () => {
-      console.log("Runs if a useEffect dependency changes.");
-      window.removeEventListener("resize", handleResize);
-    };
-    return cleanUp;
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return windowSize;
